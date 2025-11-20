485aa9a2e36f00434720c81998f8fd4d2dcef2cc
Varun Gani <120110651+varungani@users.noreply.github.com>
2025-11-20 13:52:49 +0530

    Rename file operations for clarity

--- vfs/inc/file.h
+++ vfs/inc/file.h
@@ -1,13 +1,11 @@
 #ifndef FILE_H_
 #define FILE_H_
 
-
 #include "directory.h"
 
-
 void create_file(const char *name);
-void write_file(const char *name, const char *text);
-void read_file(const char *name);
-void delete_file_node(file_node *temp);
+void write_to_file(const char *name, const char *text);
+void read_from_file(const char *name);
+void delete_file_node(file_node *file_to_delete);
 
-#endif 
+#endif
