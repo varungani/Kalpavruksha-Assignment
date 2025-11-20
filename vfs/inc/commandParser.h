16fd32ac7fb6ca5f62a848fb8930a8bd749424b0
Varun Gani <120110651+varungani@users.noreply.github.com>
2025-11-20 13:52:29 +0530

    Add new functions and remove unused ones in commandParser

--- vfs/inc/commandParser.h
+++ vfs/inc/commandParser.h
@@ -1,10 +1,8 @@
 #ifndef COMMANDPARSER_H_
 #define COMMANDPARSER_H_
 
+int calculate_string_length(const char *str);
+void parse_input_string(const char *input_string, char args_array[3][1000]);
+int is_valid_name(const char *name);
 
-
-int find_length(const char *name);
-void parse_string(const char *str, char parsed[3][1000]);
-int valid_name(const char *name);
-
-#endif 
+#endif
